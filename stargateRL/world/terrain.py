a0bfc12e1ba63fe334429ef135cb204834c2fe2c
Alexandru-Paul Copil <alexandru.p.copil@gmail.com>
2017-05-08 02:56:33 +0100

    Add overworld terrain generation algorithm draft

--- stargateRL/world/terrain.py
+++ stargateRL/world/terrain.py
@@ -1,16 +1,40 @@
 """Terrain generation and more."""
 
 
-from noise import pnoise2
+from noise import snoise2
 import random
+import math
 
 
-with open('test.pmg', 'w') as pmg_file:
-    pmg_file.write('P2\n')
-    pmg_file.write('256 256\n')
-    pmg_file.write('255\n')
+freq = 1.20
+width = 500
+height = 200
+seed = random.getrandbits(21)
 
-    freq = 0.27
-    for y in range(256):
-        for x in range(256):
-            print pnoise2(x * freq, y * freq, 2, base=random.getrandbits(21)) / 2 + 0.5
+
+def noise(nx, ny):
+    """Noise from 0.0 to 1.0."""
+    return snoise2(nx, ny, base=seed) / 2 + 0.5
+
+
+def a_noise(nx, ny, exp, *en):
+    """Advanced noise generation."""
+    final = 0.0
+    for i, e in enumerate(en):
+        final += e * noise(math.pow(2, i) * nx, math.pow(2, i) * ny)
+    final = final / sum(en)
+    return math.pow(final, exp)
+
+
+elevation = [[0 for y in range(height)] for x in range(width)]
+for y in range(height):
+    for x in range(width):
+        nx = float(x)/width - 0.5
+        ny = float(y)/height - 0.5
+        elevation[x][y] = '#' if a_noise(nx, ny, 1.0, 1.0, 0.58, 0.24, 0.19, 0.0, 0.08) > 0.5 else '.'
+
+for r in elevation:
+    s = str(r).replace(' ', '')
+    s = s.replace(',', '')
+    s = s.replace('\'', '')
+    print s
