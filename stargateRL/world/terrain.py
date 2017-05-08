e1460c2eb61ef81ddc78d748771f6f483ed164b0
Alexandru-Paul Copil <alexandru.p.copil@gmail.com>
2017-05-08 17:49:55 +0100

    New noise map generator show promise
    Add examples.zip, LOOK AT THIS GRAPH!

--- stargateRL/world/terrain.py
+++ stargateRL/world/terrain.py
@@ -1,40 +1,97 @@
 """Terrain generation and more."""
 
 
-from noise import snoise2
+from noise import pnoise2 as noise2
 import random
-import math
+from random import getrandbits
 
 
-freq = 1.20
-width = 500
-height = 200
-seed = random.getrandbits(21)
+def noise(x, y, width, height):
+    """Return noise between 0.0 and 1.0."""
+    return noise2(x, y, repeatx=width, repeaty=height) / 2.0 + 0.5
 
 
-def noise(nx, ny):
-    """Noise from 0.0 to 1.0."""
-    return snoise2(nx, ny, base=seed) / 2 + 0.5
+def normalize(min_value, max_value, value):
+    """Normalize the given value between 0.0 and 1.0."""
+    return float(value - min_value) / (max_value + min_value)
 
 
-def a_noise(nx, ny, exp, *en):
-    """Advanced noise generation."""
-    final = 0.0
-    for i, e in enumerate(en):
-        final += e * noise(math.pow(2, i) * nx, math.pow(2, i) * ny)
-    final = final / sum(en)
-    return math.pow(final, exp)
+def generate_noise_map(width, height, scale, octaves, persistance, lacunarity,
+                       seed=0):
+    """Generate a 2D map of noise."""
+    random.seed(seed)
+
+    if scale <= 0:
+        scale = 0.0001
+    if width < 1:
+        width = 1
+    if height < 1:
+        height = 1
+    if lacunarity < 1:
+        lacunarity = 1
+    if octaves < 0:
+        octaves = 0
+
+    noise_map = [[0 for y in range(height)] for x in range(width)]
+
+    max_noise = 0
+    min_noise = 0
+
+    octaves_offsets = []
+    for o in range(octaves):
+        offset_x = random.randint(-100000, 100000)
+        offset_y = random.randint(-100000, 100000)
+        octaves_offsets.append((offset_x, offset_y))
+
+    for y in range(height):
+        for x in range(width):
+
+            amplitude = 1.0
+            frequency = 1.0
+            noise_height = 0.0
+
+            for o in range(octaves):
+                sample_x = float(x - width / 2) / scale * \
+                    frequency + octaves_offsets[o][0]
+                sample_y = float(y - height / 2) / scale * \
+                    frequency + octaves_offsets[o][1]
+
+                noise_value = noise(sample_x, sample_y, width, height)
+                noise_height += noise_value * amplitude
+
+                amplitude *= persistance
+                frequency *= lacunarity
+
+            if noise_height > max_noise:
+                max_noise = noise_height
+            if noise_height < min_noise:
+                min_noise = noise_height
+
+            noise_map[x][y] = noise_height
+
+    for y in range(height):
+        for x in range(width):
+            noise_map[x][y] = normalize(min_noise, max_noise, noise_map[x][y])
+            import math
+            noise_map[x][y] = math.pow(noise_map[x][y], 7)
+
+    return noise_map
 
 
-elevation = [[0 for y in range(height)] for x in range(width)]
-for y in range(height):
-    for x in range(width):
-        nx = float(x)/width - 0.5
-        ny = float(y)/height - 0.5
-        elevation[x][y] = '#' if a_noise(nx, ny, 1.0, 1.0, 0.58, 0.24, 0.19, 0.0, 0.08) > 0.5 else '.'
+def create_pgm(noise_map, file_name):
+    """Create PGM heightmap."""
+    with open(file_name, 'w') as pmg:
+        pmg.write('P2\n')
+        pmg.write('{} {}\n'.format(len(noise_map), len(noise_map[0])))
+        pmg.write('255\n')
+        for row in noise_map:
+            for noise in row:
+                pmg.write('{}\n'.format(int(noise * 255.0)))
 
-for r in elevation:
-    s = str(r).replace(' ', '')
-    s = s.replace(',', '')
-    s = s.replace('\'', '')
-    print s
+
+# my_seed = getrandbits(21)
+my_seed = 21
+print my_seed
+create_pgm(generate_noise_map(width=1024, height=1024, scale=250.0, octaves=4,
+                              persistance=0.4, lacunarity=1.87, seed=my_seed),
+           '{}.pgm'.format(my_seed))
